f9168de64cb6af59810e9ca7ba871c771181d7cb
Caio Vinicius <viniciuscaioml@gmail.com>
2023-05-23 21:37:24 -0300

    project cfg

---
+++ HortCult-PotatosHead/HortCult_PotatosHeadApp.swift
@@ -0,0 +1,19 @@
+//
+//  HortCult_PotatosHeadApp.swift
+//  HortCult-PotatosHead
+//
+//  Created by userext on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HortCult_PotatosHeadApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
