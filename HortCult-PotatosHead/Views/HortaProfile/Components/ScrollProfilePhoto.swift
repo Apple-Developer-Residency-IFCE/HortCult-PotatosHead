97a561701dd765493f7c3c7fe22b0a7f973a6969
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-05-29 18:56:57 -0300

    criando componente de imagem scrolled

---
+++ HortCult-PotatosHead/Views/HortaProfile/Components/ScrollProfilePhoto.swift
@@ -0,0 +1,32 @@
+//
+//  ScrollProfilePhoto.swift
+//  HortCult-PotatosHead
+//
+//  Created by Joao Guilherme Araujo Canuto on 26/05/23.
+//
+
+import SwiftUI
+
+struct ScrollProfilePhoto: View {
+    let images = ["Image", "Image 1", "Image 2"]
+    
+    var body: some View {
+        VStack {
+            TabView {
+                ForEach(0..<3){ i in
+                    Image("\(images[i])").resizable().ignoresSafeArea()
+                    
+                }
+            }.tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                .frame(height: 390)
+        }
+        
+    }
+}
+
+struct ScrollProfilePhoto_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollProfilePhoto()
+    }
+}
