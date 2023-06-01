300beceac3de506824507d5fd4e4763c499340a1
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-06-01 18:47:42 -0300

    fase inicial da componentização

---
+++ HortCult-PotatosHead/Views/HortaProfile/Components/FrequenciaRega.swift
@@ -0,0 +1,20 @@
+//
+//  FrequenciaRega.swift
+//  HortCult-PotatosHead
+//
+//  Created by Joao Guilherme Araujo Canuto on 01/06/23.
+//
+
+import SwiftUI
+
+struct FrequenciaRega: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrequenciaRega_Previews: PreviewProvider {
+    static var previews: some View {
+        FrequenciaRega()
+    }
+}
