17bc6eaa0d296172094aab5edb40db6a18964588
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-06-01 21:18:59 -0300

    Componente de frequencia de rega

--- HortCult-PotatosHead/Views/HortaProfile/Components/FrequenciaRega.swift
+++ HortCult-PotatosHead/Views/HortaProfile/Components/FrequenciaRega.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 
+var frequencias:[Int:String] = [
+    1: "Todos os Dias",
+    2: "A cada Dois Dias",
+    3: "A cada Três Dias",
+    4: "A cada Quatro dias",
+    5: "A cada Cinco dias",
+    6: "A cada Seis Dias",
+    7: "Semanalmente",
+    14: "A cada Duas Semamanas",
+    30: "Mensalmente"
+]
+
 struct FrequenciaRega: View {
+    var frequencia:Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text("Frêquencia de Rega")
+                .font(.custom("Satoshi-Regular", size: 16))
+                .padding(.bottom, 1)
+            Text(frequencias[frequencia]!)
+                .font(.custom("Satoshi-Regular", size: 18))
+                .bold()
+        }
     }
 }
 
 struct FrequenciaRega_Previews: PreviewProvider {
     static var previews: some View {
-        FrequenciaRega()
+        FrequenciaRega(frequencia: 1)
     }
 }
