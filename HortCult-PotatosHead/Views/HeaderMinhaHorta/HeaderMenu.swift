5d8e37258dc341c5c3a299f24777d189e5afd110
João Guilherme <joaogacanuto@gmail.com>
2023-06-13 21:12:13 -0300

    Corrigindo HeaderMenu

--- HortCult-PotatosHead/Views/HeaderMinhaHorta/HeaderMenu.swift
+++ HortCult-PotatosHead/Views/HeaderMinhaHorta/HeaderMenu.swift
@@ -8,36 +8,26 @@
 import SwiftUI
 
 struct HeaderMenu: View {
-    @State private var isOnInformationPage = false
-//    var body: some View {
-//        VStack(){
-//            HStack {
-//                Text("Minha Horta")
-//                    .font(.system(size: 16))
-//                    .bold()
-//                    .foregroundColor(Color("MainColor"))
-//                Spacer()
-//                Button(action: {})
-//                {
-//                    Image(systemName: "plus")
-//                        .foregroundColor(Color("backgroundColor"))
-//                }
-//                .frame(width: 32,height: 32, alignment: .center)
-//                .background(Color("MainColor"))
-//                .clipShape(Circle())
-//            }
-//            .padding(.bottom,10)
-//            ListHorta()
-//        }
-//    }
     var body: some View {
-        NavigationStack {
-            NavigationLink {
-                VStack{
-                    HortaInformation()
+        NavigationView{
+            VStack{
+                HStack {
+                    Text("Minha Horta")
+                        .font(.system(size: 16))
+                        .bold()
+                        .foregroundColor(Color("MainColor"))
+                    Spacer()
+                    Button(action: {})
+                    {
+                        Image(systemName: "plus")
+                            .foregroundColor(Color("backgroundColor"))
+                    }
+                    .frame(width: 32,height: 32, alignment: .center)
+                    .background(Color("MainColor"))
+                    .clipShape(Circle())
                 }
-            } label: {
-                HortaComponent(imagePath: "Tomate", nameHorta: "Tomate")
+                .padding(.bottom,10)
+                ListHorta()
             }
         }
     }
