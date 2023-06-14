06256b68cba39177b530da81082953783ed09732
João Guilherme <joaogacanuto@gmail.com>
2023-06-13 21:21:08 -0300

    Corrigindo HeaderMenu

--- HortCult-PotatosHead/Views/HeaderMinhaHorta/HeaderMenu.swift
+++ HortCult-PotatosHead/Views/HeaderMinhaHorta/HeaderMenu.swift
@@ -9,25 +9,26 @@ import SwiftUI
 
 struct HeaderMenu: View {
     var body: some View {
-        VStack(){
-            HStack {
-                Text("Minha Horta")
-                    .font(.custom("Satoshi-Bold", size: 26))
-                    .bold()
-                    .foregroundColor(Color("MainColor"))
-                Spacer()
-                Button(action: {})
-                {
-                    Image(systemName: "plus")
-                        .foregroundColor(Color("backgroundColor"))
-                        
+        NavigationView{
+            VStack{
+                HStack {
+                    Text("Minha Horta")
+                        .font(.system(size: 16))
+                        .bold()
+                        .foregroundColor(Color("MainColor"))
+                    Spacer()
+                    Button(action: {})
+                    {
+                        Image(systemName: "plus")
+                            .foregroundColor(Color("backgroundColor"))
+                    }
+                    .frame(width: 32,height: 32, alignment: .center)
+                    .background(Color("MainColor"))
+                    .clipShape(Circle())
                 }
                 .padding(.bottom,10)
                 ListHorta()
             }
-            .padding(.horizontal)
-            .padding(.bottom,10)
-            ListHorta()
         }
     }
 }
