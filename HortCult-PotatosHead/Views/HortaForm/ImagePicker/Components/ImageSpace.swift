05b7e0f0928cdba06f091c11fc22d67a0516d2d2
João Guilherme <joaogacanuto@gmail.com>
2023-06-15 20:50:50 -0300

    Inicializando

---
+++ HortCult-PotatosHead/Views/HortaForm/ImagePicker/Components/ImageSpace.swift
@@ -0,0 +1,20 @@
+//
+//  ImageSpace.swift
+//  HortCult-PotatosHead
+//
+//  Created by Joao Guilherme Araujo Canuto on 15/06/23.
+//
+
+import SwiftUI
+
+struct ImageSpace: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageSpace_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageSpace()
+    }
+}
---
+++ HortCult-PotatosHead/Views/HortaForm/ImagePicker/ImagePickerComponent.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerComponent.swift
+//  HortCult-PotatosHead
+//
+//  Created by Joao Guilherme Araujo Canuto on 14/06/23.
+//
+
+import SwiftUI
+
+struct ImagePickerComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerComponent()
+    }
+}
