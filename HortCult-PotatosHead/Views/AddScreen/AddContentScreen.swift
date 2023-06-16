5723c1494d5a908a8cd34df9892ff551555b1351
Caio Vinicius <viniciuscaioml@gmail.com>
2023-06-16 19:16:33 -0300

    montando tela

---
+++ HortCult-PotatosHead/Views/AddScreen/AddContentScreen.swift
@@ -0,0 +1,22 @@
+//
+//  AddContentScreen.swift
+//  HortCult-PotatosHead
+//
+//  Created by Caio Vinicius on 16/06/23.
+//
+
+import SwiftUI
+
+struct AddContentScreen: View {
+    var body: some View {
+        VStack{
+            TextScreen()
+        }
+    }
+}
+
+struct AddContentScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AddContentScreen()
+    }
+}
