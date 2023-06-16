39a8371772a7b4c8977d83af77a48c9212483fd1
João Guilherme <joaogacanuto@gmail.com>
2023-06-15 21:33:17 -0300

    Inicializando os alerts
    Inicializando os alerts
    
    Co-Authored-By: Caio Vinicius <106553681+caio-vmelo@users.noreply.github.com>
    Co-Authored-By: carlosamorim28 <81266635+carlosamorim28@users.noreply.github.com>
    Co-Authored-By: GabrielCarvalhoM <101423069+GabrielCarvalhoM@users.noreply.github.com>

---
+++ HortCult-PotatosHead/Views/Alerts/AlertComponent.swift
@@ -0,0 +1,20 @@
+//
+//  AlertComponent.swift
+//  HortCult-PotatosHead
+//
+//  Created by Joao Guilherme Araujo Canuto on 15/06/23.
+//
+
+import SwiftUI
+
+struct AlertComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlertComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertComponent()
+    }
+}
