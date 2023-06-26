7199a091e307af708aac4f61affd46d03e464b50
Caio Vinicius <viniciuscaioml@gmail.com>
2023-06-26 18:52:11 -0300

    Merge branch 'apresentacao' of github.com:Apple-Developer-Residence-IFCE/HortCult-PotatosHead into feature/MockEditScreen

--- HortCult-PotatosHead/Views/Textfield/DescriptionTextfield.swift
+++ HortCult-PotatosHead/Views/Textfield/DescriptionTextfield.swift
@@ -9,41 +9,49 @@ struct DescriptionTextfield: View {
             Text("Descrição")
                 .font(.custom("Satoshi-Regular", size: 12))
                 .foregroundColor(Color("buttonCardColor"))
-            VStack{
-                if text.isEmpty {
-                    TextEditor(text: $text)
-                        .padding(.leading)
-                        .foregroundColor(.gray)
-                        .opacity(0.5)
-                        .frame(height: 200)
-                        .overlay(
-                            Text(placeholder)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .font(.custom("Satoshi-Regular", size: 16))
-                                .foregroundColor(Color("hourButton"))
-                                .padding(.vertical, -90)
-                                .padding(.horizontal, 20)
-                        )
-                } else {
-                    TextEditor(text: $text)
-                        .padding(.leading)
-                        .foregroundColor(.primary)
-                }
-            }
-            .frame(height: 200)
-            .cornerRadius(10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.gray, lineWidth: 1)
-            )
+                .padding(.horizontal, 8)
+            
+            TextEditorWrapper(text: $text, placeholder: placeholder)
+                .frame(height: 200)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
         }
         .padding(.horizontal, 20)
     }
 }
 
-struct DescriptionTextfield_Previews: PreviewProvider {
-    static var previews: some View {
-        DescriptionTextfield(text: .constant(""), placeholder: "Insria uma descrição")    }
+struct TextEditorWrapper: View {
+    @Binding var text: String
+    var placeholder: String
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                placeholderText
+            }
+            
+            TextEditor(text: $text)
+                .foregroundColor(.primary)
+                .opacity(text.isEmpty ? 0.5 : 1)
+                .padding(.leading)
+        }
+    }
+    
+    var placeholderText: some View {
+        Text(placeholder)
+            .font(.custom("Satoshi-Regular", size: 16))
+            .foregroundColor(Color("hourButton"))
+            .padding(.vertical, 12)
+            .padding(.horizontal, 20)
+    }
 }
 
+struct DescriptionTextfield_Previews: PreviewProvider {
+    static var previews: some View {
+        DescriptionTextfield(text: .constant(""), placeholder: "Insira uma descrição")
+    }
+}
 
