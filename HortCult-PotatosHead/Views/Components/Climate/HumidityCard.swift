915b2f49185123d01897eb8904a9548065aef9b1
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-06 20:23:21 -0300

    Creating-the-climate-cards

--- HortCult-PotatosHead/Views/Components/Climate/HumidityCard.swift
+++ HortCult-PotatosHead/Views/Components/Climate/HumidityCard.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 import SwiftUI
 
-struct ClimateCard: View {
+struct HumidityCard: View {
     let title: String = "Umidade"
     let content: String
-    let icon: String = "Water-Blue"
-    let cardColor: String = "BlueAlertCard"
     let backgroudCardColor: String = "HumidityCardColor"
     let textColor: String = "HumidityTextColor"
     let titleFont: String = "Satoshi-Italic"
@@ -22,14 +20,17 @@ struct ClimateCard: View {
     var body: some View {
         HStack{
             VStack(alignment: .leading, spacing: 3){
-                HStack(alignment: .center, spacing: 2){
-                    Image("Umidade-Icon").foregroundColor(Color("HumidityTextColor"))
+                HStack(alignment: .center, spacing: 7){
+                    Image("Umidade-Icon")
+                        .resizable()
+                        .frame(width: 17, height: 17)
+                        .foregroundColor(Color("HumidityTextColor"))
                     Text(title)
                         .font(.custom(titleFont, size: 11))
                 }
                 Text(content)
                     .font(.custom(contentFont, size: 20.97))
-                    .padding(.leading, 5)
+                    .padding(.leading, 2)
             }
             .foregroundColor(Color(textColor))
             .padding(.leading, 12)
@@ -43,8 +44,8 @@ struct ClimateCard: View {
     }
 }
 
-struct ClimateCard_Previews: PreviewProvider {
+struct HumidityCard_Previews: PreviewProvider {
     static var previews: some View {
-        ClimateCard(content: "60%")
+        HumidityCard(content: "60%")
     }
 }
--- HortCult-PotatosHead/Views/Components/Climate/TemperatureCard.swift
+++ HortCult-PotatosHead/Views/Components/Climate/TemperatureCard.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct TemperatureCard: View {
+    let title: String = "Temperatura"
+    let content: String
+    let backgroudCardColor: String = "TemperatureCardColor"
+    let textColor: String = "TemperatureTextColor"
+    let titleFont: String = "Satoshi-Italic"
+    let contentFont: String = "Satoshi-Bold"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading, spacing: 3){
+                HStack(alignment: .center, spacing: 7){
+                    Image("Sun-Icon")
+                        .resizable()
+                        .frame(width: 17, height: 17)
+                        .foregroundColor(Color("TemperatureTextColor"))
+                    Text(title)
+                        .font(.custom(titleFont, size: 11))
+                }
+                Text(content)
+                    .font(.custom(contentFont, size: 20.97))
+                    .padding(.leading, 2)
+            }
+            .foregroundColor(Color(textColor))
+            .padding(.leading, 12)
+            Spacer()
+        }
+        .frame(width: 125,height: 65)
+        .background(
+            Color(backgroudCardColor)
+        )
+        .cornerRadius(8)
     }
 }
 
 struct TemperatureCard_Previews: PreviewProvider {
     static var previews: some View {
-        TemperatureCard()
+        TemperatureCard(content: "30ºC")
     }
 }
