5843de107d32d83ec38d36e89e93109de0adffc1
Yohanne Moreira <yohannemoreira@alu.ufc.br>
2023-07-06 21:26:12 -0300

    Cria Alerts

---
+++ HortCult-PotatosHead/Views/Alerts/AlertCustomView.swift
@@ -0,0 +1,215 @@
+//
+//  AlertCustomView.swift
+//  HortCult-PotatosHead
+//
+//  Created by Yohanne Moreira on 03/07/23.
+//
+
+import SwiftUI
+
+
+enum alertas {
+    case add
+    case delete
+    case confirmDelete
+    case deleteAdd
+}
+
+
+struct AlertCustomView: View {
+    @State var alerta : alertas
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var body: some View {
+        ZStack {
+            if alerta == .add {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 260, height: 147)
+                .foregroundColor(Color("BackgroundComponent"))
+            
+            VStack(spacing: 0){
+                VStack{
+                    Text("Planta cadastrada!")
+                        .font(.custom("Satoshi-Regular", size: 16))
+                        .foregroundColor(Color("MainColor"))
+                        .bold()
+                        .padding(.top, 17)
+                    
+                    Text("""
+                         Você pode ver sua planta diretamente
+                               na tela inicial, em \"Minha Horta\"
+                         """)
+                    .font(.custom("Satoshi-Regular", size: 12))
+                    .foregroundColor(Color("TextColor"))
+                    .padding(.top, 1.5)
+                    .padding(.bottom, 14)
+                    
+                    Divider().frame(width: 260)
+                }
+                
+                HStack{
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text("Tela inicial")
+                            .font(.custom("Satoshi-Regular", size: 16))
+                            .foregroundColor(Color("TextColor"))
+                    }).frame(minWidth: 130, minHeight: 22)
+                    
+                    Divider().frame(height: 44)
+                    //                        .padding(.bottom, 6)
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text("Ver planta")
+                            .font(.custom("Satoshi-Regular", size: 16))
+                            .foregroundColor(Color("tabBarSelectedItem"))
+                            .bold()
+                    }).frame(minWidth: 130, minHeight: 22)
+                    
+                }.frame(width: 260, height: 45)
+            }
+        }
+            
+            if alerta == .confirmDelete{
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 260, height: 147)
+                    .foregroundColor(Color("BackgroundComponent"))
+                
+                VStack(spacing: 0){
+                    VStack{
+                        Text("Deseja excluir essa planta? ")
+                            .font(.custom("Satoshi-Regular", size: 16))
+                            .foregroundColor(Color("MainColor"))
+                            .bold()
+                            .padding(.top, 17)
+                        
+                        Text(" Essa ação não poderá ser desfeita")
+                        .font(.custom("Satoshi-Regular", size: 12))
+                        .foregroundColor(Color("TextColor"))
+                        .padding(.top, 1.5)
+                        .padding(.bottom, 14)
+                        
+                        Divider().frame(width: 260)
+                    }
+                    
+                    HStack(alignment: .center){
+                        Button(action: {
+                            // code
+                        }, label: {
+                            Text("Cancelar")
+                                .font(.custom("Satoshi-Regular", size: 16))
+                                .foregroundColor(Color("TextColor"))
+                        }).frame(minWidth: 130, minHeight: 22)
+                        
+                        Divider().frame(height: 44)
+                        //                        .padding(.bottom, 6)
+                        Button(action: {
+                            // code
+                        }, label: {
+                            Text("Excluir")
+                                .font(.custom("Satoshi-Regular", size: 16))
+                                .foregroundColor(Color("red"))
+                                .bold()
+                        }).frame(minWidth: 130, minHeight: 22)
+                        
+                    }.frame(width: 260, height: 45)
+                }
+            
+            }
+            
+            if alerta == .delete {
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 260, height: 111)
+                    .foregroundColor(Color("BackgroundComponent"))
+                
+                VStack(spacing: 0){
+                    VStack{
+                        Text("Planta excluída!")
+                            .font(.custom("Satoshi-Regular", size: 16))
+                            .foregroundColor(Color("MainColor"))
+                            .bold()
+                            .padding(.vertical, 10)
+    
+                        Divider().frame(width: 260)
+                    }
+                    
+                    HStack(alignment: .center){
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }, label: {
+                            Text("Voltar para Tela Inicial")
+                                .font(.custom("Satoshi-Regular", size: 16))
+                                .foregroundColor(Color("TextColor"))
+                        }).frame(minWidth: 130, minHeight: 22)
+                        
+        
+                        
+                    }.frame(width: 260, height: 45)
+                }
+            
+            }
+            
+            if alerta == .deleteAdd {
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 260, height: 147)
+                    .foregroundColor(Color("BackgroundComponent"))
+                
+                VStack(spacing: 0){
+                    VStack{
+                        Text("""
+                             Deseja descartar a criação
+                                    da sua planta?
+                             """)
+                            .font(.custom("Satoshi-Regular", size: 16))
+                            .foregroundColor(Color("MainColor"))
+                            .bold()
+                            .padding(.top, 17)
+                        
+                        Text(" Essa ação não poderá ser desfeita")
+                        .font(.custom("Satoshi-Regular", size: 12))
+                        .foregroundColor(Color("TextColor"))
+                        .padding(.top, 1.5)
+                        .padding(.bottom, 14)
+                        
+                        Divider().frame(width: 260)
+                    }
+                    
+                    HStack {
+                        Button(action: {
+                            // code
+                        }, label: {
+                            Text("Cancelar")
+                                .font(.custom("Satoshi-Regular", size: 16))
+                                .foregroundColor(Color("TextColor"))
+                        }).frame(minWidth: 130, minHeight: 22)
+                        
+                        Divider().frame(height: 44)
+                        //                        .padding(.bottom, 6)
+                        Button(action: {
+                            // code
+                        }, label: {
+                            Text("Descartar")
+                                .font(.custom("Satoshi-Regular", size: 16))
+                                .foregroundColor(Color("red"))
+                                .bold()
+                        }).frame(minWidth: 130, minHeight: 22)
+                        
+                    }.frame(width: 260, height: 45)
+                }
+            
+            }
+        }
+            
+       
+    }
+}
+
+struct AlertCustomView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertCustomView(alerta: .deleteAdd)
+    }
+}
--- HortCult-PotatosHead/Views/Pages/AddInfoScreen/AddInfoScreen.swift
+++ HortCult-PotatosHead/Views/Pages/AddInfoScreen/AddInfoScreen.swift
@@ -17,35 +17,26 @@ struct AddInfoScreen: View {
     var plant: Plant?
     @ObservedObject var plantViewModel: PlantViewModel
     @State var isEdit: Bool = false
+    @State private var presentAlert = false
+    @State private var addAlert = false
+    @State private var editAlert = false
+    //
     
-    var header: some View {
-        ZStack{
-            Image(defaults.theme ==  "Escuro" ? "Topbardark" : "Topbar")
-            HStack{
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                } ) {
-                    Image("leftArrow")
-                }
-                .padding(.leading, 12)
-                Spacer()
-            }
-        }
-    }
     var body: some View {
-        VStack{
-            CustomNavBar(hiddenDismissButton: false)
-                ZStack{
+
+            ZStack{
+                VStack{
+                    CustomNavBar(hiddenDismissButton: false)
                     
                     ScrollView{
                         
                         HStack {
-                        Text(isEdit ? "Editar Informações" : "Adicionar Vegetal")
-                            .font(.custom("Satoshi-Bold", size: 28))
-                            .foregroundColor(Color("MainColor"))
-                        Spacer()
-                    }
-                    .padding()
+                            Text(isEdit ? "Editar Informações" : "Adicionar Vegetal")
+                                .font(.custom("Satoshi-Bold", size: 28))
+                                .foregroundColor(Color("MainColor"))
+                            Spacer()
+                        }
+                        .padding()
                         TextScreen(text: $nameText, description: $descriptionText, isEdit: false)
                             .padding(.bottom, 20)
                         CategoryDropDownView(selectedOption: $category)
@@ -54,71 +45,90 @@ struct AddInfoScreen: View {
                             .padding(.bottom, 20)
                         ImagePickerComponentView(selectedPhotosData: $selectedPhotosData)
                             .padding(.bottom, 110)
+                        
+                        
                     }
                     
-                    VStack{
-                        Spacer()
-                        if (!isEdit){
-                            if (!((frequency != nil) && (category != nil) && !nameText.isEmpty && !descriptionText.isEmpty)){
-                                AddButton(isDisabled: true){}
-                            } else {
-                                AddButton(isDisabled: false) {
-                                    guard let frequencia = frequency?.rawValue else {return}
-                                    guard let categoria = category?.rawValue else {return}
-                                    guard let neewPlant  = plantViewModel.createPlant(name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia) else{return}
-                                    selectedPhotosData.forEach { Data in
-                                        guard let newImage = imageViewModel.createImage(plantImage: Data) else {return}
-                                        plantViewModel.addImageToPlant(plant: neewPlant, plantImage: newImage)
-                                    }
-                                    
-                                    guard let newNotification = notificationViewModel.createNotification(next_time_to_alert: notificationViewModel.calculateNextWatering(wateringFrequency: frequency!), time_to_alert: "", type_to_alert: NotificationType.watering.rawValue) else {return}
-                                    plantViewModel.addNotificationToPlant(plant: neewPlant, notification: newNotification)
-                                    self.presentationMode.wrappedValue.dismiss()
-                                    
+                }.edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    Spacer()
+                    if (!isEdit){
+                        if (!((frequency != nil) && (category != nil) && !nameText.isEmpty && !descriptionText.isEmpty)){
+                            AddButton(isDisabled: true){}
+                        } else {
+                            AddButton(isDisabled: false) {
+                                guard let frequencia = frequency?.rawValue else {return}
+                                guard let categoria = category?.rawValue else {return}
+                                guard let neewPlant  = plantViewModel.createPlant(name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia) else{return}
+                                selectedPhotosData.forEach { Data in
+                                    guard let newImage = imageViewModel.createImage(plantImage: Data) else {return}
+                                    plantViewModel.addImageToPlant(plant: neewPlant, plantImage: newImage)
+                                
                                 }
+                                addAlert = true
+                                guard let newNotification = notificationViewModel.createNotification(next_time_to_alert: notificationViewModel.calculateNextWatering(wateringFrequency: frequency!), time_to_alert: "", type_to_alert: NotificationType.watering.rawValue) else {return}
+                                plantViewModel.addNotificationToPlant(plant: neewPlant, notification: newNotification)
+                                
                                 
                             }
-                        } else {
-                            if (!((frequency != nil) && (category != nil) && !nameText.isEmpty && !descriptionText.isEmpty)){
-                                EditButton(isDisabled: true){}
-                            }
-                            else {
-                                EditButton(isDisabled: false){
-                                    if(!isEdit){
-                                        guard let frequencia = frequency?.rawValue else {return}
-                                        guard let categoria = category?.rawValue else {return}
-                                        guard plantViewModel.createPlant(name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia) != nil else {return}
-                                        self.presentationMode.wrappedValue.dismiss()
-                                    } else {
-                                        
-                                        guard let frequencia = frequency?.rawValue else {return}
-                                        guard let categoria = category?.rawValue else {return}
-                                        guard let plant = plant else {return}
-                                        
-                                        plantViewModel.updatePlant(plant: plant, name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia)
-                                        
-                                        plant.plant_hortcult_images?.allObjects.forEach({ image in
-                                            plantViewModel.removeImageToPlant(plant: plant, plantImage: (image as! Hortcult_Images))
-                                        })
-                                        
-                                        selectedPhotosData.forEach { Data in
-                                            guard let newImage = imageViewModel.createImage(plantImage: Data) else {return}
-                                            plantViewModel.addImageToPlant(plant: plant, plantImage: newImage)
-                                        }
-                                        self.presentationMode.wrappedValue.dismiss()
+                            
+                        }
+                    } else {
+                        if (!((frequency != nil) && (category != nil) && !nameText.isEmpty && !descriptionText.isEmpty)){
+                            EditButton(isDisabled: true){}
+                        }
+                        else {
+                            EditButton(isDisabled: false){
+                                if(!isEdit){
+                                    guard let frequencia = frequency?.rawValue else {return}
+                                    guard let categoria = category?.rawValue else {return}
+                                    guard plantViewModel.createPlant(name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia) != nil else {return}
+                                    self.presentationMode.wrappedValue.dismiss()
+                                } else {
+                                    
+                                    guard let frequencia = frequency?.rawValue else {return}
+                                    guard let categoria = category?.rawValue else {return}
+                                    guard let plant = plant else {return}
+                                    
+                                    plantViewModel.updatePlant(plant: plant, name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia)
+                                    
+                                    plant.plant_hortcult_images?.allObjects.forEach({ image in
+                                        plantViewModel.removeImageToPlant(plant: plant, plantImage: (image as! Hortcult_Images))
+                                    })
+                                    
+                                    selectedPhotosData.forEach { Data in
+                                        guard let newImage = imageViewModel.createImage(plantImage: Data) else {return}
+                                        plantViewModel.addImageToPlant(plant: plant, plantImage: newImage)
                                     }
+                                    self.presentationMode.wrappedValue.dismiss()
                                 }
                             }
                         }
-                        }
                     }
-                    .padding(.bottom, 60)
                 }
+                if presentAlert {
+                    Color.black.opacity(0.1)
+                        .edgesIgnoringSafeArea(.all)
+                        .zIndex(0)
+                }
+                
+                if addAlert {
+                    AlertCustomView(alerta: .add)
+                        .zIndex(1)
+                        .onAppear{
+                            presentAlert = true
+                        }
+                }
+                
+            }
+            .padding(.bottom, 60)
+            
+        
         .edgesIgnoringSafeArea(.all)
         .navigationBarBackButtonHidden(true)
         
-            
-     
+        
     }
 }
 
