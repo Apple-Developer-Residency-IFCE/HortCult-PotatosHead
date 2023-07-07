6947953706f63b577633e25450e23b2fbf2ed234
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-06 21:41:11 -0300

    creating-ClimateScreen

--- HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct ClimateScreen: View {
+    let cityInfo: String
+    let dateInfo: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            CustomNavBar(hiddenDismissButton: true)
+            Spacer().frame(height: 20)
+            
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Clima")
+                        .foregroundColor(Color("MainColor"))
+                        .font(.custom("Satoshi-Bold", size: 24))
+                    Spacer().frame(height: 12)
+                    
+                    HStack{
+                        Text(cityInfo)
+                            .foregroundColor(Color("MainColor"))
+                            .font(.custom("Satoshi-Medium", size: 14))
+                        
+                        Text(dateInfo)
+                            .foregroundColor(Color("MainColor"))
+                            .font(.custom("Satoshi-Regular", size: 12))
+                    }
+                    HStack{
+                        TemperatureCard(content: "30ºC")
+                        HumidityCard(content: "60%")
+                    }
+                }
+                .padding(.leading, 20)
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
 
 struct ClimateScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ClimateScreen()
+        ClimateScreen(cityInfo: "Fortaleza, CE", dateInfo: "Qui, 28 de Fevereiro")
     }
 }
