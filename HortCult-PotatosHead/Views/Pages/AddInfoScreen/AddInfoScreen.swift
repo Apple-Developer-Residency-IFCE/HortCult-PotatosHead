07e116216c19500fa869a949d126f7cd3b96a413
Yohanne Moreira <yohannemoreira@alu.ufc.br>
2023-07-17 21:42:47 -0300

    Corrige AddInfoScreen

--- HortCult-PotatosHead/Views/Pages/AddInfoScreen/AddInfoScreen.swift
+++ HortCult-PotatosHead/Views/Pages/AddInfoScreen/AddInfoScreen.swift
@@ -65,12 +65,13 @@ struct AddInfoScreen: View {
                                     guard let frequencia = frequency?.rawValue else {return}
                                     guard let categoria = category?.rawValue else {return}
                                     guard let neewPlant  = Service.plant.createPlant(name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia) else{return}
-                                    selectedPhotosData.forEach { Data in
-                                        guard let newImage = Service.image.createImage(plantImage: Data) else {return}
+                                    selectedPhotosData.forEach { data in
+                                        guard let newImage = Service.image.createImage(plantImage: data) else {return}
                                         Service.plant.addImageToPlant(plant: neewPlant, plantImage: newImage)
                                     }
                                     
-                                    guard let newNotification = Service.notification.createNotification(next_time_to_alert: Service.notification.calculateNextWatering(wateringFrequency: frequency!), time_to_alert: "", type_to_alert: NotificationType.watering.rawValue) else {return}
+                                    guard let newNotification = Service.notification.createNotification(next_time_to_alert: Service.notification.calculateNextWatering(wateringFrequency: frequency!),
+                                                                                                        time_to_alert: "", type_to_alert: NotificationType.watering.rawValue) else {return}
                                     Service.plant.addNotificationToPlant(plant: neewPlant, notification: newNotification)
                                     self.presentationMode.wrappedValue.dismiss()
                                     let notificationDisplayed = HomeViewModel.notificationsTextsToDisplay(notification: newNotification)
@@ -107,11 +108,12 @@ struct AddInfoScreen: View {
                                         Service.plant.updatePlant(plant: plant, name: nameText, category: categoria , information: descriptionText, watering_frequency: frequencia)
                                         
                                         plant.plant_hortcult_images?.allObjects.forEach({ image in
-                                            Service.plant.removeImageToPlant(plant: plant, plantImage: (image as! Hortcult_Images))
+                                            guard let imagePlant = image as? Hortcult_Images else {return}
+                                            Service.plant.removeImageToPlant(plant: plant, plantImage: imagePlant)
                                         })
                                         
-                                        selectedPhotosData.forEach { Data in
-                                            guard let newImage = Service.image.createImage(plantImage: Data) else {return}
+                                        selectedPhotosData.forEach { data in
+                                            guard let newImage = Service.image.createImage(plantImage: data) else {return}
                                             Service.plant.addImageToPlant(plant: plant, plantImage: newImage)
                                         }
                                         self.presentationMode.wrappedValue.dismiss()
