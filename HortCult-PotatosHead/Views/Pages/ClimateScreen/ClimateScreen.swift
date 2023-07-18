6ff37824029c591e76f4935c652721fbf39859c6
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-18 18:18:34 -0300

    finish-ClimateScreen

--- HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
@@ -1,10 +1,3 @@
-//
-//  ClimateScreen.swift
-//  HortCult-PotatosHead
-//
-//  Created by Caio Vinicius on 06/07/23.
-//
-
 import SwiftUI
 
 struct ClimateScreen: View {
@@ -14,34 +7,30 @@ struct ClimateScreen: View {
     var body: some View {
         VStack{
             CustomNavBar(hiddenDismissButton: true)
-            Spacer().frame(height: 20)
-            
             HStack{
-                VStack(alignment: .leading){
+                VStack(alignment: .leading, spacing: 20) {
                     Text("Clima")
                         .foregroundColor(Color("MainColor"))
                         .font(.custom("Satoshi-Bold", size: 24))
-                    Spacer().frame(height: 12)
-                    
-                    HStack{
+                    HStack {
                         Text(cityInfo)
                             .foregroundColor(Color("MainColor"))
                             .font(.custom("Satoshi-Medium", size: 14))
-                        
                         Text(dateInfo)
                             .foregroundColor(Color("MainColor"))
                             .font(.custom("Satoshi-Regular", size: 12))
                     }
-                    HStack{
+                    HStack {
                         TemperatureCard(content: "30ºC")
                         HumidityCard(content: "60%")
                     }
                 }
-                .padding(.leading, 20)
+                .padding()
                 Spacer()
             }
             Spacer()
         }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
@@ -50,3 +39,4 @@ struct ClimateScreen_Previews: PreviewProvider {
         ClimateScreen(cityInfo: "Fortaleza, CE", dateInfo: "Qui, 28 de Fevereiro")
     }
 }
+
