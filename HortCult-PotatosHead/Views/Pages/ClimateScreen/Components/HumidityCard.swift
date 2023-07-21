bce0606307f6bb3c5c6c7440659d2fe98718019d
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-21 20:21:50 -0300

    creating-humidity-card

--- HortCult-PotatosHead/Views/Pages/ClimateScreen/Components/HumidityCard.swift
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/Components/HumidityCard.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct HumidityCard: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+     let title: String = "Umidade"
+     let content: String
+     let backgroudCardColor: String = "HumidityCardColor"
+     let textColor: String = "HumidityTextColor"
+     let titleFont: String = "Satoshi-Italic"
+     let contentFont: String = "Satoshi-Bold"
 
-struct HumidityCard_Previews: PreviewProvider {
-    static var previews: some View {
-        HumidityCard()
-    }
-}
+     var body: some View {
+         HStack {
+             VStack(alignment: .leading, spacing: 3) {
+                 HStack(alignment: .center, spacing: 7) {
+                     Image("Umidade-Icon")
+                         .resizable()
+                         .frame(width: 17, height: 17)
+                         .foregroundColor(Color("HumidityTextColor"))
+                     Text(title)
+                         .font(.custom(titleFont, size: 11))
+                 }
+                 Text(content)
+                     .font(.custom(contentFont, size: 20.97))
+                     .padding(.leading, 2)
+             }
+             .foregroundColor(Color(textColor))
+             .padding(.leading, 12)
+             Spacer()
+         }
+         .frame(width: 125, height: 65)
+         .background(
+             Color(backgroudCardColor)
+         )
+         .cornerRadius(8)
+     }
+ }
+
+ struct HumidityCard_Previews: PreviewProvider {
+     static var previews: some View {
+         HumidityCard(content: "60%")
+     }
+ }
