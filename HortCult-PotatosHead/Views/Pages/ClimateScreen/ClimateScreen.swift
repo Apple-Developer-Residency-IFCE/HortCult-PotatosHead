59856a6c31cb52b2759f1707a2c144b5f9e52e2c
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-21 20:21:29 -0300

    creating-necessary-assets

---
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ClimateScreen.swift
+//  HortCult-PotatosHead
+//
+//  Created by Caio Vinicius on 21/07/23.
+//
+
+import SwiftUI
+
+struct ClimateScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClimateScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ClimateScreen()
+    }
+}
---
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/Components/HumidityCard.swift
@@ -0,0 +1,20 @@
+//
+//  HumidityCard.swift
+//  HortCult-PotatosHead
+//
+//  Created by Caio Vinicius on 21/07/23.
+//
+
+import SwiftUI
+
+struct HumidityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HumidityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HumidityCard()
+    }
+}
