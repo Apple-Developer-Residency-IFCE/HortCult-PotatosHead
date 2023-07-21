9de6034fcfeccbf5f6685e0f8bc68883ef22189c
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-21 18:40:16 -0300

    fixing-the-image-service-tests

--- HortCult-PotatosHeadTests/ImageServiceTests.swift
+++ HortCult-PotatosHeadTests/ImageServiceTests.swift
@@ -14,40 +14,47 @@ final class ImageServiceTests: XCTestCase {
         try super.setUpWithError()
         imageService = ImageService.instance
     }
-
     override func tearDownWithError() throws {
-       imageService = nil
         try super.tearDownWithError()
+        imageService = nil
     }
     func testCreateImage() throws {
-        let image = ImageService.instance.createImage(plantImage: Data())
+        let image = imageService.createImage(plantImage: Data())
         XCTAssertNotNil(image)
         XCTAssertNotNil(image?.id)
         XCTAssertNotNil(image?.plant_image)
     }
 
     func testDeleteImage() throws {
-        let image = ImageService.instance.createImage(plantImage: Data())
-        ImageService.instance.deleteImage(plantImage: image!)
-        let fetchedImages = ImageService.instance.hortcultImages
+        let image = imageService.createImage(plantImage: Data())
+        imageService.deleteImage(plantImage: image!)
+        let fetchedImages = imageService.hortcultImages
         XCTAssertFalse(fetchedImages.contains(image!))
     }
 
     func testUpdateImage() throws {
-        // Create an image.
-        let image = ImageService.instance.createImage(plantImage: Data())
+        // Criando uma imagem e convertendo-a para Data, formato o qual é o esperado.
+        let image = UIImage(named: "Tomate")
+        let imageData = image?.pngData()
 
-        // Get the image ID.
-        let imageID = image?.id
+        // Criando uma nova imagem a partir do método createImage.
+        let newImage = imageService.createImage(plantImage: imageData!)
 
-        // Update the image.
-        let updatedImage = Data()
-        ImageService.instance.updateImage(plantImage: imageID, updatedImage: updatedImage)
+        // Validação para ver se a imagem foi criada.
+        XCTAssertNotNil(newImage, "Failed to create a new image.")
 
-        // Fetch the updated image.
-        let fetchedImage = ImageService.instance.hortcultImages.first(where: { $0.id == imageID })
+        // Criando uma nova imagem para utilizar o método de update.
+        let updatedImage = UIImage(named: "Abobrinha")
+        let updatedImageData = updatedImage?.pngData()
 
-        // Check that the updated image is correct.
-        XCTAssertEqual(fetchedImage?.plant_image, updatedImage)
+        // Aplicando o método a ser testado
+        imageService.updateImage(plantImage: newImage!, updatedImage: updatedImageData!)
+
+        // Buscando a imagem pelo id para verificar se ela está realmente no contexto.
+        let updatedImageFromCoreData = imageService.hortcultImages.first { $0.id == newImage?.id }
+
+        // Checando se a imagem realmente foi criada e se é a mesma criada no banco.
+        XCTAssertNotNil(updatedImageFromCoreData, "Imagem atualizada não foi encontrada no contexto")
+        XCTAssertEqual(updatedImageFromCoreData?.plant_image, updatedImageData, "A imagem não conseguiu ser atualizada")
     }
 }
