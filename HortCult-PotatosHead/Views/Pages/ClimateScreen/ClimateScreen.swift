b932b2fbc63ef1f8b63e95417ed26735eab20734
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-21 21:35:58 -0300

    again

---
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/ClimateScreen.swift
@@ -0,0 +1,48 @@
+//
+//  ClimateScreen.swift
+//  HortCult-PotatosHead
+//
+//  Created by Caio Vinicius on 21/07/23.
+//
+
+import SwiftUI
+
+ struct ClimateScreen: View {
+     let cityInfo: String
+     let dateInfo: String
+
+     var body: some View {
+         VStack {
+             CustomNavBar(hiddenDismissButton: true)
+             HStack {
+                 VStack(alignment: .leading, spacing: 20) {
+                     Text("Clima")
+                         .foregroundColor(Color("MainColor"))
+                         .font(.custom("Satoshi-Bold", size: 24))
+                     HStack {
+                         Text(cityInfo)
+                             .foregroundColor(Color("MainColor"))
+                             .font(.custom("Satoshi-Medium", size: 14))
+                         Text(dateInfo)
+                             .foregroundColor(Color("MainColor"))
+                             .font(.custom("Satoshi-Regular", size: 12))
+                     }
+                     HStack {
+                         TemperatureCard(content: "30ºC")
+                         HumidityCard(content: "60%")
+                     }
+                 }
+                 .padding()
+                 Spacer()
+             }
+             Spacer()
+         }
+         .ignoresSafeArea(.all, edges: .top)
+     }
+ }
+
+ struct ClimateScreen_Previews: PreviewProvider {
+     static var previews: some View {
+         ClimateScreen(cityInfo: "Fortaleza, CE", dateInfo: "Qui, 28 de Fevereiro")
+     }
+ }
---
+++ HortCult-PotatosHead/Views/Pages/ClimateScreen/Components/TemperatureCard.swift
@@ -0,0 +1,49 @@
+//
+//  TemperatureCard.swift
+//  HortCult-PotatosHead
+//
+//  Created by Caio Vinicius on 21/07/23.
+//
+
+import SwiftUI
+
+struct TemperatureCard: View {
+     let title: String = "Temperatura"
+     let content: String
+     let backgroudCardColor: String = "TemperatureCardColor"
+     let textColor: String = "TemperatureTextColor"
+     let titleFont: String = "Satoshi-Italic"
+     let contentFont: String = "Satoshi-Bold"
+
+     var body: some View {
+         HStack {
+             VStack(alignment: .leading, spacing: 3) {
+                 HStack(alignment: .center, spacing: 7) {
+                     Image("Sun-Icon")
+                         .resizable()
+                         .frame(width: 17, height: 17)
+                         .foregroundColor(Color("TemperatureTextColor"))
+                     Text(title)
+                         .font(.custom(titleFont, size: 11))
+                 }
+                 Text(content)
+                     .font(.custom(contentFont, size: 20.97))
+                     .padding(.leading, 2)
+             }
+             .foregroundColor(Color(textColor))
+             .padding(.leading, 12)
+             Spacer()
+         }
+         .frame(width: 125, height: 65)
+         .background(
+             Color(backgroudCardColor)
+         )
+         .cornerRadius(8)
+     }
+ }
+
+ struct TemperatureCard_Previews: PreviewProvider {
+     static var previews: some View {
+         TemperatureCard(content: "30ºC")
+     }
+ }
