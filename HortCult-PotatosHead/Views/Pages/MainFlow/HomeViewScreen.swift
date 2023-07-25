1778f032cc575e4087ea67cb90aa3ff19ca45859
Caio Vinicius <viniciuscaioml@gmail.com>
2023-07-25 20:49:50 -0300

    uncouple-the-HomeViewScreen

--- HortCult-PotatosHead/Views/Pages/MainFlow/HomeViewScreen.swift
+++ HortCult-PotatosHead/Views/Pages/MainFlow/HomeViewScreen.swift
@@ -5,7 +5,6 @@
 //  Created by userext on 30/05/23.
 //
 
-
 import SwiftUI
 
 struct HomeView: View {
@@ -15,7 +14,7 @@ struct HomeView: View {
     @State var mokeRemainderList: [Notification] = []
     @State var cardModels: [CardViewModel] = []
     var body: some View {
-        ScrollView(.vertical){
+        ScrollView(.vertical) {
             VStack {
                 CustomNavBar(hiddenDismissButton: true)
                 Spacer()
@@ -24,25 +23,13 @@ struct HomeView: View {
                     Spacer().frame(height: Service.plant.plants.isEmpty ? 120 : 0)
                 }
                 Spacer().frame(height: Service.plant.plants.isEmpty ? 120 : 0)
-                .padding(.bottom, 20)
-                
+                    .padding(.bottom, 20)
                 CardListView(cards: $cardModels)
             }
-            .onAppear() {
+            .onAppear {
                 cardModels = []
-                let remindersList = Service.notification.notifications.compactMap({ notification in
-                    notification
-                }).filter { notification in
-                    let currentDate = Date()
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "dd/MM/yyyy"
-                    var dateString = dateFormatter.string(from: currentDate)
-                    
-                    return (!notification.is_resolve && notification.next_time_to_alert == dateString && notification.notification_plant != nil)
-                }
-                
+                let remindersList = HomeViewModel.getReminderCardByDate()
                 remindersList.forEach { notification in
-                    
                     let notificationDisplayed = HomeViewModel.notificationsTextsToDisplay(notification: notification)
                     let cardModel: CardViewModel = CardViewModel(
                         id: notificationDisplayed.id,
@@ -57,11 +44,12 @@ struct HomeView: View {
                 }
             }
         }
-        .background(NavigationLink(destination: AddInfoScreen(noticationList: $cardModels), isActive: $goToAddPlantScreen, label: {EmptyView()}))
+        .background(NavigationLink(
+            destination: AddInfoScreen(noticationList: $cardModels),
+            isActive: $goToAddPlantScreen, label: {EmptyView()}))
         .navigationBarBackButtonHidden(true)
         .edgesIgnoringSafeArea(.all)
     }
-    
 }
 
 struct HomeView_Previews: PreviewProvider {
@@ -69,36 +57,4 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
             .environmentObject(Defaults())
     }
-}
-
-struct NotificationAdapter {
-    
-    var id: UUID? = UUID()
-    var isResolve: Bool? = false
-    var nextTimeToAlert: String? = ""
-    var timeToAlert: String? = ""
-    var typeToAlert: String? = ""
-    var notificationPlant: PlantAdapter? = PlantAdapter()
-    
-    init() {}
-    
-    init(notification: Notification) {
-        self.id = notification.id
-        self.isResolve = notification.is_resolve
-        self.nextTimeToAlert = notification.next_time_to_alert
-        self.timeToAlert = notification.time_to_alert
-        self.typeToAlert = notification.type_to_alert
-        self.notificationPlant = PlantAdapter(category: notification.notification_plant?.category,id: notification.notification_plant?.id,name: notification.notification_plant?.name)
-    }
-}
-
-struct PlantAdapter {
-    var category: String? = ""
-    var id: UUID? = UUID()
-    var information: String? = ""
-    var name: String? = "Jninho"
-    var wateringFrequency: String? = ""
-    var plantNotification: [NotificationAdapter]? = []
-    var plantHortcultImages: [HortCultImages]? = []
-    
 }
